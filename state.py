4717c561c62658cf17683accbf1d41a6fa987a3b
xingyue <xingyue@xsec.com>
2018-12-11 10:57:08 +0800

    添加栈，队列两种结构

--- state.py
+++ state.py
@@ -8,3 +8,22 @@ def set(k,v):
 def get(k):
     return kvdb[k]
 
+#### LILO 后进后出
+def nlist(k,v):
+    kvdb[k] = [v]
+
+def nadd(k,v):
+    kvdb[k].append(v)
+
+def nshift(k):
+    return kvdb[v].popleft()
+
+### LIFO Last In First Out
+def nstack(k,v):
+    kvdb[k] = [v]
+
+def npush(k,v):
+    kvdb[k].append(v)
+
+def npop(k):
+    kvdb[k].append(v)
